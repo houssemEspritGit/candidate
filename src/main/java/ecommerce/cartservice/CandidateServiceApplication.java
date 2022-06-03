020fa19e4857c97313c6c6bf2fa039b4c79b1914
Houssem Zerai <houssem.zeraii@digit-u.com>
2022-06-03 20:47:12 +0100

    initial commit

---
+++ src/main/java/ecommerce/cartservice/CandidateServiceApplication.java
@@ -0,0 +1,12 @@
+package ecommerce.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateServiceApplication.class, args);
+	}
+}
