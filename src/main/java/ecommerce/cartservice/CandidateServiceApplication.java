99125bb0bd7675710d6666bab39dd5742fd54735
Houssem Zerai <houssem.zeraii@digit-u.com>
2022-06-03 21:30:42 +0100

    force push

--- src/main/java/ecommerce/cartservice/CandidateServiceApplication.java
+++ src/main/java/ecommerce/cartservice/CandidateServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CandidateServiceApplication {
-//force pus
+//force pu
 	public static void main(String[] args) {
 		SpringApplication.run(CandidateServiceApplication.class, args);
 	}
